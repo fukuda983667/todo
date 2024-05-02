678f5ff3a9bdf930f4a37b2b58ba7d4b7ae4364a
fukuda <sfu983667@gmail.com>
2024-05-02 11:57:02 +0900

    2-2 マイグレーション、モデル、リレーション設定

--- src/app/Models/Category.php
+++ src/app/Models/Category.php
@@ -5,16 +5,11 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Todo extends Model
+class Category extends Model
 {
     use HasFactory;
 
     protected $fillable = [
-        'content'
+        'name'
     ];
-
-    public function categories()
-    {
-        return $this->belongsTo('App\Models\Category');
-    }
 }
